047ec561b7c6b0ae0d070bd62e01412109a16ce8
andrewperon <58343499+andrewperon@users.noreply.github.com>
2019-12-05 17:07:47 -0600

    add URL for key generation

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-# enter your API credentials below
+# enter your API credentials below - generate at https://my.telegram.org/auth
 api_id = YOUR_API_ID
 api_hash = 'YOUR_API_HASH'
 phone = '+15555551234'
