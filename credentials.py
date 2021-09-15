6b17d7b23125cfa28e7da5538c01905c2df3dd73
Andrew Peron <andrew.peron@gmail.com>
2021-09-14 20:46:14 -0500

    comment for phone string input

--- credentials.py
+++ credentials.py
@@ -1,4 +1,7 @@
 # enter your API credentials below - generate at https://my.telegram.org/auth
 api_id = YOUR_API_ID
 api_hash = 'YOUR_API_HASH'
+
+# phone number MUST include country code and + prefix
+
 phone = '+15555551234'
